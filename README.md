c74d8a8e0ed108e69896ac50a483d8d75ac41c90
SrBaliardo <tao.john007@icloud.com>
2025-01-24 08:15:45 -0300

    feat(code): added README.

---
+++ README.md
@@ -0,0 +1,24 @@
+## Mudar o tema não é nenhum bicho de sete cabeças
+
+Este foi um dos primeiros componentes desenvolvidos antes de dar o próximo passo rumo ao React!
+
+<br>
+
+## Changing the theme is no big deal
+
+This was one of the first components developed before taking the next step towards React!
+
+<br>
+
+You can view the project via the link: <br>
+https://srbaliardo.github.io/toggle-theme/
+
+<br>
+
+<img align="center" alt="HTML" height="300" width="400" src="./images/animation_toggle-theme.gif">
+
+<br>
+
+" <img align="center" alt="HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"> |
+<img align="center" alt="CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"> |
+<img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"> "
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const btnToggle = document.querySelector('.btn-toggle');
-const container = document.querySelector('.container');
-const h1 = document.querySelector('h1');
-const moon = document.querySelector('.moon');
-const sun = document.querySelector('.sun');
+const btnToggle = document.querySelector(".btn-toggle");
+const container = document.querySelector(".container");
+const h1 = document.querySelector("h1");
+const moon = document.querySelector(".moon");
+const sun = document.querySelector(".sun");
 
-btnToggle.addEventListener('click', function() {
-    this.classList.toggle('active')
-    container.classList.toggle('active')
-    h1.classList.toggle('active')
-    moon.classList.toggle('active')
-    sun.classList.toggle('active')
+btnToggle.addEventListener("click", function () {
+  this.classList.toggle("active");
+  container.classList.toggle("active");
+  h1.classList.toggle("active");
+  moon.classList.toggle("active");
+  sun.classList.toggle("active");
 });
